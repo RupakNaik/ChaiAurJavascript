958f31ce182ee4313bf941289a9f7a1205200bc1
rupak <naik.rupak31@gmail.com>
2024-08-17 19:28:19 +0530

    operations

---
+++ 01_basics/03_01_operations.js
@@ -0,0 +1,56 @@
+// let value =2;
+// let negativeValue = -value;
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+// console.log(2<<2);
+// console.log(2>>2);
+// console.log(2>>>2);
+// console.log(2|2);
+// console.log(2&2);
+// console.log(2^2);
+// console.log(2==2);
+// console.log(2!=2);
+// console.log(2>2);
+// console.log(2<2);
+// console.log(2>=2);
+// console.log(2<=2);
+// console.log(2===2);
+// console.log(2!==2);
+// console.log(2 && 2);
+// console.log(2 || 2);
+// console.log(2 ? 2 : 2);
+// console.log(2 ? 2 : 3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+
+// console.log(2+"1"+2);
+
+// console.log(2+"1"+"1");
+
+// console.log(2+1+"1");
+
+// console.log(true);
+// console.log(+true);
+
+// let gameCounter = 100;
+// gameCounter++
+
+// console.log(gameCounter);
+
+//https://tc39.es/ecma262/#sec-type-conversion
+
+let motu = 4;
+const patalu = motu++;
+
+console.log(`motu: ${motu}, patalu: ${patalu}`);
+
+let a = 3;
+const b = ++a
+console.log(`a: ${a}, b: ${b}`);
