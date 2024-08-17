f7e08ae1d973a4a428806345a9311658a77e6e14
rupak <naik.rupak31@gmail.com>
2024-08-17 14:50:34 +0530

    data types in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" //use all JS code as newer version
+
+//alert(3+2); // this will not work as we are using nodeJs not a browser to execute
+
+//string => ""
+//number => 2 to pwoer 53
+//bigint
+//boolean => true/false
+//null => standalone value
+//undefined => variable declared but not assigned any value
+//object => collection of key value pairs
+//symbol => unique and immutable
+
+console.log(typeof "12"); // string
+console.log(typeof 12); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
