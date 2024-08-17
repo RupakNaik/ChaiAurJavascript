41b53bb34b7707feb020002298a70bd4ea252366
rupak <naik.rupak31@gmail.com>
2024-08-17 14:20:35 +0530

    JS variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 1234;
+let accountName = "Rupak";
+var accountPassword = "121212";
+accountCity = "Mumbai"
+let accountState;
+
+console.table([accountId,accountName,accountPassword,accountCity, accountState]);
+
+// preffer not to use "var" because of issue in block scope and functional scope
